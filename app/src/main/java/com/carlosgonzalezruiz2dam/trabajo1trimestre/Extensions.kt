a7f5b2ca8e619bf68e7e81bd25e8781051a846d4
Carlos-Gonzalez-Ruiz <104576450+Carlos-Gonzalez-Ruiz@users.noreply.github.com>
2022-12-09 10:39:43 +0100

    Update Extensions.kt

--- app/src/main/java/com/carlosgonzalezruiz2dam/trabajo1trimestre/Extensions.kt
+++ app/src/main/java/com/carlosgonzalezruiz2dam/trabajo1trimestre/Extensions.kt
@@ -4,13 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.Toast
 import com.bumptech.glide.Glide
-
-//requireContext().toast("Hola")
-fun Context.toast(mensaje: String){
-    Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
-}
 
 fun ViewGroup.inflate(layout: Int, attachToRoot: Boolean = true) =
     LayoutInflater.from(context).inflate(layout, this, attachToRoot)
